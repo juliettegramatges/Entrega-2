0ad20d4f39262510f1b268868cb469ed06d9fc74
Juliette Gramatges <juliettegramatges@200-33-187-eduroam-al.wlan.net.uc.cl>
2024-10-11 08:53:05 -0300

    correccion asignaturas

--- cargador.php
+++ cargador.php
@@ -1,7 +1,21 @@
 <?php
-
+# filtrar base original
 require 'cargador_asignatura.php'; 
 procesar_asignaturas();
+
+require 'cargador_docentes_planificados.php'; 
+procesar_docentes_planificados();
+
+
+# corregir bases datos filtrados
+
+require 'cargador_asignaturas_malas.php'; 
+corregir_asignaturas();
+
+require 'cargador_docentes_malos.php'; 
+corregir_asignaturas();
+
+
 
 
 ?>
